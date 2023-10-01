a69f73921fe7a1d1e4444218a8b1dbb35e35139c
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-10-01 18:53:20 -0400

    adds functions for search history functionality in javascript file

--- script.js
+++ script.js
@@ -1,25 +1,77 @@
 //DOM ELEMMENTS ========================
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var todayContainer = document.querySelector('#today');
+var forecastContainer = document.querySelector('#forecast');
+var searchHistoryContainer = document.querySelector('#history');
 
 //DATA==================================
-var cityName = document.getElementById("#search-input");
-console.log(cityName);
-var buttonElement = document.querySelector("#search-button");
+var searchHistory = [];
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var weatherApiKey = '';
+var currentLatitude = 0;
+var currentLongitude = 0;
 //FUNCTIONS=============================
-function handleClick(event) {
-  fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q={cityName}&limit=5&appid=f2482696e807d323d0d132ef26887c56"
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      // Process the API response data here
-      console.log(data);
-    })
-    .catch((error) => {
-      // Handle errors here
-      console.error("Error:", error);
-    });
+
+dayjs.extended(window.dayjs_plugin_utc);
+dayjs.extended(window.dayjs_plugin_timezone);
+
+function renderSearchHistory() {
+  searchHistoryContainer.innerHTML = '';
+
+for (var i = searchHistory.length - 1; i >= 0; i--) {
+  var btn = document.createElement('button');
+  btn.setAttribute('type', 'button');
+  btn.setAttribute('aria-controls', 'today forecast');
+  btn.classList.add('history-btn', 'btn-history');
+
+btn.setAttribute('data-search', searchHistory[i]);
+btn.textContent = searchHistory[i];
+searchHistoryContainer.append(btn);
+
 }
 
+}
+
+function appendToHistory(search) {
+  if (searchHistory.indexOf(search) !== -1) {
+    return;
+  }
+  searchHistory.push(search);
+  localStorage.setItem('search-history', JSON.stringify(searchHistory));
+  renderSearchHistory();
+}
+
+function renderCurrentWeather( city, weather) {
+var date = dayjs().format('M/D/YYY');
+var tempF = weather.main.temp;
+var windMph = weather.wind.speed;
+var humidity = weather.main.humidity;
+var iconUrl = `https://openweathermap.org/img/w${weather.weather[0].icon}.png`;
+var iconDescription = weather.weather[0].description || weather[0].main;
+
+var card = document.createElement('div');
+var cardBody = document.createElement('div');
+var heading = document.createElement('h2');
+var weatherIcon = document.createElement('img');
+var tempEl = document.createElement('p');
+var windEl = document.createElement('p');
+var humidityEl = document.createElement('p');
+
+card.setAttribute('class', 'card');
+cardBody.setAttribute('class', 'card-body');
+card.append(cardBody);
+
+heading.setAttribute('class', 'h3 card-title');
+heading.setAttribute('class', 'h3 card-title');
+heading.setAttribute('class', 'h3 card-title');
+heading.setAttribute('class', 'h3 card-title');
+heading.setAttribute('class', 'h3 card-title');
+heading.setAttribute('class', 'h3 card-title');
+
+
+
+}
 //USER INTERACTIONS======================
-buttonElement.addEventListener("click", handleClick);
-//INITALIZATIONS=========================
+
+//INITIALIZATIONS=========================
